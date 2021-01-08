5a1eb264d0d0cbdbc0bffbcc5785bf6959ccb8c8
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-01-08 15:30:44 +0300

    wpf registration

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleAppFive
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ExitProgram_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TimeNewRomanFont_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Times New Roman");
+            timeNewRomanFont.IsChecked = false;
+        }
+
+        private void VerdanaFont_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Verdana");
+            verdanaFont.IsChecked = false;
+        }
+
+        private void saveFile_Click(object sender, RoutedEventArgs e)
+        {
+            saveToFile();
+        }
+
+        private void createNewFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                saveToFile();
+            }
+            textBox.Text = "";
+        }
+
+        private void saveToFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            bool? res = sfd.ShowDialog();
+            if (res != false)
+            {
+
+                using (Stream s = File.Open(sfd.FileName, FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(textBox.Text);
+                    }
+                }
+            }
+        }
+
+        private void openNewFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            bool? res = ofd.ShowDialog();
+            if (res != false)
+            {
+                Stream myStream;
+                if ((myStream = ofd.OpenFile()) != null)
+                {
+                    string file_name = ofd.FileName;
+                    string file_text = File.ReadAllText(file_name);
+                    textBox.Text = file_text;
+                }
+            }
+        }
+
+        private void selectFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = selectFontSize.SelectedItem.ToString();
+            fontSize = fontSize.Substring(fontSize.Length - 2);
+            switch (fontSize)
+            {
+                case "10":
+                    textBox.FontSize = 10;
+                    break;
+                case "14":
+                    textBox.FontSize = 14;
+                    break;
+                case "16":
+                    textBox.FontSize = 16;
+                    break;
+                case "20":
+                    textBox.FontSize = 20;
+                    break;
+                case "24":
+                    textBox.FontSize = 24;
+                    break;
+                case "32":
+                    textBox.FontSize = 32;
+                    break;
+                case "48":
+                    textBox.FontSize = 48;
+                    break;
+            }
+        }
+
+        private void createNewFile_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
